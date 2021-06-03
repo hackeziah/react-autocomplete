9f4ed3d220dafd170e6da7fc5ae53d4ab850367c
hackeziah <lamadridkevinpaul@gmail.com>
2021-06-04 01:55:30 +0800

    autocomplete search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 function App() {
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(()=>{
+    const loadUsers = async() => {
+      const response = await axios.get('https://reqres.in/api/users');
+      setUsers(response.data.data)
+    }
+    loadUsers()
+  },[])
+
+  const onChangeHandeler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+        matches = users.filter(user=>{
+          const regex = new RegExp(`${text}`, "gi")
+          return user.email.match(regex)
+        })
+    }
+
+    setSuggestions(matches)
+    setText(text)
+  
+  }
+  const onSuggestHandler = (suggested)=>{
+    setText(suggested)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <input type="text"name="test" className="col-md-12 input" style={{marginTop:10}}
+      onChange= {e=>onChangeHandeler(e.target.value)} value={text}/>
+
+    {suggestions && suggestions.map((item, i)=>
+      <div key={i} className="col-md-12" onClick={()=>onSuggestHandler(item.email)}> {item.email}
+       </div>
+      
+    )}
     </div>
   );
 }
